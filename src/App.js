236fa7f74bd3f97eae550898ada64315b37f0dd1
Alexander Griffin <alexander.griffin99@hotmail.com>
2018-03-08 23:41:54 -0500

    fixed gh-pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
+        <BrowserRouter basename={ process.env.PUBLIC_URL }>
           <AppContainer />
         </BrowserRouter>
       </div>
