158817ff6e9e7f823d16107af8e8bcccafee1c31
Alexander Griffin <alexander.griffin99@hotmail.com>
2018-03-15 05:34:49 -0400

    d

---
+++ src/components/EmoteError.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import themes from "../assets/js/theme.js"
+
+
+
+export default class EmoteError extends Component {
+  constructor(props) {
+    super(props)
+    this.emotes = [
+      "( 　ﾟ,_ゝﾟ)",
+      "¯\\_(ツ)_/¯",
+      "Σ(ﾟДﾟ；≡；ﾟдﾟ)",
+      "щ（ﾟДﾟщ",
+      "ಠ╭╮ಠ",
+      "ಥ﹏ಥ",
+      "⊙▃⊙",
+      "◉︵◉",
+      "(╯°□°）╯︵ ┻━┻",
+      "(ﾟДﾟ)",
+    ]
+  }
+  render() {
+    return (
+      <div className="error">
+        <p style={{ ...this.props.style }}>
+          { this.emotes[Math.floor(Math.random() * this.emotes.length)] }
+        </p>
+        { this.props.children }
+      </div>
+    )
+  }
+}
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,6 +6,7 @@ import api from "../assets/js/api.js"
 import themes from "../assets/js/theme.js"
 
 import SearchBar from "./SearchBar.js"
+import EmoteError from "./EmoteError.js"
 
 
 class ProjectList extends Component {
@@ -43,12 +44,32 @@ class ProjectList extends Component {
         padding: "16px 32px",
         marginBottom: "16px",
         fontSize: "32px",
+      },
+      placeholder: {
+        textAlign: "center",
+        opacity: "0.4",
+        color: themes[this.state.theme].color,
+        fontSize: "48px",
+        fontWeight: "300"
+      },
+      placeholderLink: {
+        transition: "all 0.3s cubic-bezier(.25,.8,.25,1)",
+        ":hover": {
+          opacity: "1",
+
+        }
       }
     }
   }
 
   filterList(e) {
-
+    let filteredList = {}
+    for(let key in this.state.list) {
+      if(this.state.list[key].name.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1) {
+        filteredList[key] = this.state.list[key]
+      }
+    }
+    this.setState({filteredList})
   }
 
   render() {
@@ -75,11 +96,16 @@ class ProjectList extends Component {
                    theme={ this.state.theme }
                    style={ this.style.gridItem }/>
         {(list.length > 0 && list) || (
-          <div>
-            <p>you have no projects <br/>
-              <Link to="/app/edit/new/default/">Why not create one?</Link>
-            </p>
-          </div>
+          <EmoteError style={ this.style.placeholder }>
+            <p style={this.style.placeholder}>no projects found</p>
+            <br />
+            <Link to="/app/edit/new/default">
+              <p style={{ ...this.style.placeholder,
+                          ...this.style.placeholderLink }}>
+                why not create one?
+              </p>
+            </Link>
+          </EmoteError>
         )}
       </div>
     );
