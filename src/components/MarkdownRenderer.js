f9850c93f4765e6f8c2316540d7756c35816bf47
Alexander Griffin <alexander.griffin99@hotmail.com>
2018-03-25 14:08:36 -0400

    d

--- src/components/MarkdownRenderer.js
+++ src/components/MarkdownRenderer.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import showdown from 'showdown';
+import "showdown-prettify"
+import hljs from "highlight.js"
 
 import themes from "../assets/js/theme.js"
 import api from "../assets/js/api.js"
 
-export default class Editor extends Component {
+export default class MarkdownRenderer extends Component {
   constructor(props) {
     super(props);
     this.state = {
       markdown: props.markdown,
       converter: new showdown.Converter({
+        extensions: ['prettify'],
         strikethrough: true,
         emoji: true,
         parseImgDimensions: true,
@@ -18,7 +21,6 @@ export default class Editor extends Component {
         tasklists: true,
         underline: true,
         splitAdjacentBlockquotes: true,
-
       }),
       theme: api.getSetting("theme"),
     }
@@ -30,24 +32,31 @@ export default class Editor extends Component {
       }
     }
   }
+
   componentWillReceiveProps(nextProps) {
     if(nextProps.markdown !== this.props.markdown) {
       this.setState({markdown: nextProps.markdown})
     }
   }
+
   componentDidUpdate() {
     this.renderer.innerHTML = this.state.converter.makeHtml(this.state.markdown)
+    const codeBlocks = this.renderer.querySelectorAll("pre code");
+    for(let i = 0; i < codeBlocks.length; i++) {
+      codeBlocks[i].innerHTML = hljs.highlight(codeBlocks[i].classList[0], codeBlocks[i].innerHTML).value
+    }
   }
+
   componentWillMount() {
     let converter = {...this.state.converter}
     converter.setFlavor('github');
     this.setState({ converter })
-
   }
+
   componentDidMount() {
     this.renderer.innerHTML = this.state.converter.makeHtml(this.state.markdown)
-    console.log(this.state.converter.getOptions());
   }
+
   render() {
     return (
       <div className={"markdown-renderer " + this.state.theme}
