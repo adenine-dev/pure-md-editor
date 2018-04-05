e3352d2f8ecc598f082e6b2aec50baf0365261f4
Alexander Griffin <alexander.griffin99@hotmail.com>
2018-04-05 17:07:36 -0400

    at this point i have no clue what i am fucking doing

--- src/components/CleditEditor.js
+++ src/components/CleditEditor.js
@@ -1,23 +1,11 @@
 import React, { Component } from 'react';
-import CodeMirror from 'codemirror';
+import cledit from '../assets/js/cledit/index.js';
 import { StyleSheet, css } from 'aphrodite/no-important';
-
-import "../assets/js/codemirror/addon/composition-mod.js"
-// import "codemirror-composition-mod/  lib/codemirror-composition-mod.css"
-
-import '../assets/js/codemirror/mode/gfm.js';
-
-import '../assets/js/codemirror/addon/continuelist.js';
-import '../assets/js/codemirror/addon/overlay.js';
-import '../assets/js/codemirror/addon/closebrackets.js';
-import '../assets/js/codemirror/addon/placeholder.js';
+import Prism from 'prismjs';
+import loadLanguages from 'prismjs/components/index.js';
 
 
-import 'codemirror/mode/javascript/javascript.js';
-// import 'codemirror/mode/clike/clike.js';
-// import 'codemirror/mode/css/css.js';
-
-import keymaps from "../assets/js/codemirror/keymap/keymap.js"
+// import keymaps from "../assets/js/codemirror/keymap/keymap.js"
 
 
 
@@ -39,47 +27,55 @@ export default class CleditEditor extends Component {
     })
   }
   componentDidMount() {
-    let editor = CodeMirror(this.cm, {
-      mode: {
-        name: "gfm",
-        highlightFormatting: true,
-        ignoreUnderscore: true,
-        taskLists: true,
-        fencedCodeBlocks: true,
-        underline: true,
-      },
-      value: this.state.value,
-      placeholder: "write something",
-      autofocus: true,
-      addModeClass: true,
-      indentUnit: 2,
-      tabSize: 2,
-      enableCompositionMod: true,
-      autoCloseBrackets: true,
-      lineWrapping: true,
-      enableCompositionMod: true,
-      inputStyle: "contenteditable",
-      extraKeys: {
-        "Enter": "newlineAndIndentContinueMarkdownList",
-      },
-    })
+    const editor = cledit(this.editor, this.scroller);
+    loadLanguages(["markdown"])
+    editor.init({
+      content: this.state.value,
+      unusedthing: console.log(Prism.languages),
+      sectionHighlighter: section => Prism.highlight(section.text, Prism.languages["markdown"]),
+    });
 
-    let keymap = {};
-    for(let i = 0; i < keymaps.length; i++) {
-      if(keymaps[i].cm) {
-        keymap[keymaps[i].name] = keymaps[i].action
-      }
-    }
-    if(this.props.onChange) {
-      editor.on("change", (cm) => {
-        this.props.onChange(cm)
-      })
-    }
+    // let _editor = CodeMirror(this.cm, {
+    //   mode: {
+    //     name: "gfm",
+    //     highlightFormatting: true,
+    //     ignoreUnderscore: true,
+    //     taskLists: true,
+    //     fencedCodeBlocks: true,
+    //     underline: true,
+    //   },
+    //   value: this.state.value,
+    //   placeholder: "write something",
+    //   autofocus: true,
+    //   addModeClass: true,
+    //   indentUnit: 2,
+    //   tabSize: 2,
+    //   enableCompositionMod: true,
+    //   autoCloseBrackets: true,
+    //   lineWrapping: true,
+    //   enableCompositionMod: true,
+    //   inputStyle: "contenteditable",
+    //   extraKeys: {
+    //     "Enter": "newlineAndIndentContinueMarkdownList",
+    //   },
+    // })
+    //
+    // let keymap = {};
+    // for(let i = 0; i < keymaps.length; i++) {
+    //   if(keymaps[i].cm) {
+    //     keymap[keymaps[i].name] = keymaps[i].action
+    //   }
+    // }
+    // if(this.props.onChange) {
+    //   editor.on("change", (cm) => {
+    //     this.props.onChange(cm)
+    //   })
+    // }
+    //
+    // editor.addKeyMap( keymap );
+    // this.setState({ editor })
 
-    editor.addKeyMap( keymap );
-    this.setState({ editor })
-
-    window.mc = editor
+    window.rotide = editor
 
     if(this.props.onMount) {
       this.props.onMount(this.state, editor)
@@ -91,9 +87,11 @@ export default class CleditEditor extends Component {
   render() {
     return (
       <div className={ css([this.style.editorWrapperStyle, this.props.style]) + " editor"} >
-        <div id="main-editor"
-             className={ this.state.theme }
-             ref={(cm) => { this.cm = cm }}></div>
+        <div ref={(scroller) => { this.scroller = scroller }}>
+          <div id="main-editor"
+               className={ this.state.theme }
+               ref={(editor) => { this.editor = editor }}></div>
+        </div>
       </div>
     );
   }
