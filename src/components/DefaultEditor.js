51735f304e7bf0c1c8783a4b1f5b7fc0f0ee7c9e
alex griffin <alexander.griffin99@hotmail.com>
2018-04-09 14:40:39 -0400

    fixed save on mobile toolbar also lifted Toolbar component to Editor component

--- src/components/DefaultEditor.js
+++ src/components/DefaultEditor.js
@@ -3,7 +3,6 @@ import { Redirect } from 'react-router-dom';
 import { StyleSheet, css } from 'aphrodite/no-important';
 
 import CodeMirrorEditor from "./CodeMirrorEditor.js"
-import Toolbar from "./Toolbar.js"
 
 import api from "../assets/js/api.js"
 import { themes } from "../assets/js/theme.js"
@@ -19,25 +18,7 @@ export default class DefaultEditor extends Component {
       project: props.project,
       theme: api.getSetting("theme"),
       cm: null,
-      showToolbar: api.getSetting("showToolbar")
     }
-    this.style = StyleSheet.create({
-      toobarButton: {
-        color: themes[this.state.theme].color,
-        width: "56px",
-        backgroundColor: "transparent",
-        verticalAlign: "middle",
-        height: "100%",
-        display: "inline-flex",
-        alignItems: "center",
-        justifyContent: "center",
-
-      },
-      toobarIcon: {
-        verticalAlign: "middle",
-        height: "100%"
-      }
-    })
   }
 
   handleCmChange(cm) {
@@ -55,33 +36,16 @@ export default class DefaultEditor extends Component {
 
   getCMEditor(state, editor) {
     this.setState({ cm: editor })
+    if(this.props.onMount) {
+      this.props.onMount(editor)
+    }
   }
 
   render() {
-    let toolbar;
-    if(api.isMobile() && this.state.showToolbar) {
-      const actions = keymaps.map((action, i) => (
-        <button key={ i }
-                onClick={ (e) => {
-                  this.state.cm.focus();
-                  action.action(this.state.cm, this.state, this, {}, {})
-                } }
-                className={ css(this.style.toobarButton) }>
-          <i className={"material-icons " + css(this.style.toobarIcon)}>
-            { action.icon }
-          </i>
-        </button>
-      ))
-      toolbar = (
-        <Toolbar>
-          { actions }
-        </Toolbar>
-      )
-    }
+
 
     return (
       <div className="default-editor">
-        { toolbar }
         <CodeMirrorEditor defaultValue={ this.state.project.value }
                           onChange={ this.handleCmChange.bind(this) }
                           onMount={ this.getCMEditor.bind(this) }/>
